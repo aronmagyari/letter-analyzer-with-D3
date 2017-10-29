e2000fe5b79260476d5e991a069d1ba6850287db
Ferencz Magyari <afmagyari@gmail.com>
2017-10-28 22:04:22 -0700

    create graph based on first submit

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// write your code here!
+d3.select('#form')
+	.on('submit', function() {
+		d3.event.preventDefault();
+		var input = d3.select('input');
+		var text = input.property('value');
+
+		d3.select('#letters')
+			.selectAll('.letter')
+			.data(getFrequencies(text))
+			.enter()
+			.append('div')
+				.classed('letter', true)
+				.style('width', '20px')
+				.style('line-height', '20px')
+				.style('margin-right', '5px')
+				.style('height', function(d) {
+					return d.count * 30 + 'px';
+				})
+				.text(function(d) {
+					return d.character;
+				});
+
+		d3.select('#phrase')
+				.text('Analysis of: ' + text);
+
+		input.property('value', '');
+	});
+
+function getFrequencies(str) {
+	var sorted = str.split("").sort();
+	var data = [];
+	for (let i = 0; i < sorted.length; i++) {
+		let last = data[data.length - 1];
+		if (last && last.character === sorted[i]) last.count++;
+		else data.push({ character: sorted[i], count: 1 });
+	}
+	return data;
+}
